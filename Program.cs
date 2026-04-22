f0bb3b5686d418145b924dd2648d62d755f25959
Ian Fairbrother <fairbrother.iang@yahoo.co.nz>
2026-04-22 13:20:32 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using CompanyApi.Dtos;
+using System.Xml.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/companies", () =>
+{
+    return Results.Ok(new CompanyDto
+    {
+        Id = 1,
+        Name = "MWNZ",
+        Description = ".. is awesome"
+    });
+})
+.WithName("companies");
+
+app.Run();
